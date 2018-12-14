bb7bcc398e31a4cda2a5d33affdf76a10e6328ab
Fran <gallosino@hotmail.com>
2018-12-14 17:05:49 +0100

    Para you

--- app/src/main/java/es/fempa/juanpomares/sockets/ClasePrincipal.java
+++ app/src/main/java/es/fempa/juanpomares/sockets/ClasePrincipal.java
@@ -1,7 +1,10 @@
 package es.fempa.juanpomares.sockets;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ClasePrincipal extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ClasePrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_clase_principal);
+    }
+
+    public void manejador(View v){
+
+        if (v.getId() == R.id.b_servidor){
+            Intent intent = new Intent(ClasePrincipal.this, ClaseServidor.class);
+            startActivity(intent);
+        }else if(v.getId() == R.id.b_cliente){
+            Intent intent = new Intent(ClasePrincipal.this, ClaseCliente.class);
+            startActivity(intent);
+        }
+
     }
 }
