299a2ec1b59c31384a6e1958ef96494df0a0287d
Fran <gallosino@hotmail.com>
2018-12-14 20:46:34 +0100

    Para you

--- app/src/main/java/es/fempa/juanpomares/sockets/ClaseCliente.java
+++ app/src/main/java/es/fempa/juanpomares/sockets/ClaseCliente.java
@@ -1,7 +1,9 @@
 package es.fempa.juanpomares.sockets;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClaseCliente extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class ClaseCliente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clase_cliente);
+    }
+
+    public void manejador(View v){
+        if (v.getId() == R.id.button5){
+            Intent intent = new Intent(ClaseCliente.this, ClasePrincipal.class);
+            startActivity(intent);
+
+        }else if(v.getId() == R.id.b_vamosAChatear) {
+            Intent intent = new Intent(ClaseCliente.this, ClaseChat.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/es/fempa/juanpomares/sockets/ClaseServidor.java
+++ app/src/main/java/es/fempa/juanpomares/sockets/ClaseServidor.java
@@ -1,12 +1,25 @@
 package es.fempa.juanpomares.sockets;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ClaseServidor extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_clase_servidor);
+    }
+
+    public void manejador(View v){
+        if (v.getId() == R.id.button4){
+            Intent intent = new Intent(ClaseServidor.this, ClasePrincipal.class);
+            startActivity(intent);
+
+        }else if(v.getId() == R.id.b_vamosAChatear2) {
+            Intent intent = new Intent(ClaseServidor.this, ClaseChat.class);
+            startActivity(intent);
+        }
     }
 }
