09f5b8b21326d8cfcec9efdd7e8b2cf2d04006ca
Edu <edu.ldtv@gmail.com>
2018-12-14 20:15:23 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/layout_clase_principal.xml

---
+++ app/src/main/java/es/fempa/juanpomares/sockets/ClaseAbout.java
@@ -0,0 +1,13 @@
+package es.fempa.juanpomares.sockets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClaseAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clase_about);
+    }
+}
