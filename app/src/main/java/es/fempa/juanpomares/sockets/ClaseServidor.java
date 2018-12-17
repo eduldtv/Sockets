14ed495de5e857802bb84bbd76907869eb7148d7
Edu <edu.ldtv@gmail.com>
2018-12-17 18:02:14 +0100

    empieza lo divertido

--- app/src/main/java/es/fempa/juanpomares/sockets/ClaseServidor.java
+++ app/src/main/java/es/fempa/juanpomares/sockets/ClaseServidor.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ClaseServidor extends AppCompatActivity {
+
+    EditText puertoServidor;
+    EditText nombreServidor;
+    int numPuertoServidor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +22,14 @@ public class ClaseServidor extends AppCompatActivity {
         Button botonAtras = (Button)findViewById(R.id.buttonServidorAtras);
         Button botonChat = (Button) findViewById(R.id.buttonServidorChat);
 
+        puertoServidor = (EditText) findViewById(R.id.plainServidorPuerto);
+        nombreServidor = (EditText) findViewById(R.id.plainServidorNombre);
+
+
         botonAtras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ClaseCliente.this, ClasePrincipal.class);
+                Intent intent = new Intent(ClaseServidor.this, ClasePrincipal.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
@@ -28,19 +39,35 @@ public class ClaseServidor extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent = new Intent(ClaseServidor.this, ClaseChat.class);
+                boolean empezarActividad = true;
+                try {
+                    numPuertoServidor = Integer.parseInt(puertoServidor.getText().toString());
+                }catch (Exception e){
+                    Toast.makeText(ClaseServidor.this, "Error puerto no válido", Toast.LENGTH_SHORT).show();
+                    empezarActividad = false;
+                }
+                if(numPuertoServidor >= 1024 && numPuertoServidor <= 65535){
+                    intent.putExtra("puerto del servidor", puertoServidor.getText());
+
+                    if(nombreServidor.length() > 0){
+                        intent.putExtra("nombre del cliente", nombreServidor.getText());
+                    } else{
+                        Toast.makeText(ClaseServidor.this, "Error no ha introducido un nombre.", Toast.LENGTH_SHORT).show();
+                        empezarActividad = false;
+                    }
+
+                } else {
+                    Toast.makeText(ClaseServidor.this, "Error puerto no válido. Introduzca un valor entre 1024 y 65535.", Toast.LENGTH_LONG).show();
+                    empezarActividad = false;
+                }
+
+                if(empezarActividad == true){
+                    startActivity(intent);
+                }
+
             }
         });
     }
 
-    public void manejador(View v){
-        if (v.getId() == R.id.button4){
-            Intent intent = new Intent(ClaseServidor.this, ClasePrincipal.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-
-        }else if(v.getId() == R.id.b_vamosAChatear2) {
-            Intent intent = new Intent(ClaseServidor.this, ClaseChat.class);
-            startActivity(intent);
-        }
-    }
 }
